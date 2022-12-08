2466da6bfe2e729d4fbbdc340c05319518c50695
Hamid Alinia <a.goodlookingboy@gmail.com>
2022-12-08 18:11:28 +0330

    shomeis stable

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import Server from '@nodeeweb/server'
 import Shop from '@nodeeweb/shop'
 import Form from '@nodeeweb/form'
-import Service from '@nodeeweb/service'
+// import Service from '@nodeeweb/service'
 // import Front from '@nodeeweb/front'
 // import pasar from './plugins/pasargad-gateway/index.js'
 // import Chat , {createChatServer} from '@nodeeweb/chat'
@@ -11,7 +11,7 @@ Server({
     entity: [
         ...Shop,
         ...Form,
-        ...Service,
+        // ...Service,
         // ...Chat
     ],
     // server: [createChatServer]
